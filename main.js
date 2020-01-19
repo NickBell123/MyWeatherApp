e64073b9a0d655aa1a1fd0144c20ee1f59f6ef29
NickBell123 <nick.bell2008@gmail.com>
2020-01-19 14:14:15 +0000

    API info displaying to page

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
-
 const api = {
     key: "4dbaf49b84d9809469c0aa09d6b1cd0c",
     baseURL: "http://api.openweathermap.org/data/2.5/",
+    unsplashKey: "33cb424db5ad6ec2ecdb527c3ba4e6cf13af4599fc8f72b9b869eba3d1ec5028",
+    unsplashURL:"https://api.unsplash.com/search/"
 }
 
 const searchBox = document.querySelector('.search-box');
@@ -17,11 +18,45 @@ function getCity (evt) {
 
 function getResults(data){
     fetch(`${api.baseURL}weather?q=${data}&units=metric&APPID=${api.key}`)
-    .then(weather => {
+    .then(function(weather) {
         return weather.json();
     }).then(displayResults);
+
+    
 }
+
+
 
 function displayResults(weather){
     console.log(weather);
+
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+
+    date.innerText = getDate(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = `${weather.weather[0].main}`;
+
+    let hiLow = document.querySelector('.current .hi-low');
+    hiLow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`
 }
+
+function getDate(d){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
+
