7b7a603c4b57c5deb1852bab3c15e11a15d84dda
NickBell123 <nick.bell2008@gmail.com>
2020-01-19 20:56:13 +0000

    unsplah images api image call added based on city input

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const api = {
     key: "4dbaf49b84d9809469c0aa09d6b1cd0c",
     baseURL: "http://api.openweathermap.org/data/2.5/",
     unsplashKey: "33cb424db5ad6ec2ecdb527c3ba4e6cf13af4599fc8f72b9b869eba3d1ec5028",
-    unsplashURL:"https://api.unsplash.com/search/"
+    unsplashURL:"https://api.unsplash.com/search/photos?"
 }
 
 const searchBox = document.querySelector('.search-box');
@@ -20,11 +20,21 @@ function getResults(data){
     fetch(`${api.baseURL}weather?q=${data}&units=metric&APPID=${api.key}`)
     .then(function(weather) {
         return weather.json();
-    }).then(displayResults);
-
+    }).then(displayResults); 
     
+    fetch(`${api.unsplashURL}query=${data}&client_id=${api.unsplashKey}`)
+    .then(function(image){
+        return image.json();
+    }).then(displayImage);
 }
 
+function displayImage(image) {
+    console.log(image.results[0].urls.regular);
+    let body = document.querySelector('body');
+    body.style.backgroundImage = `url('${image.results[0].urls.regular}')`; 
+    // `<script>body{background: "url('${image.results[0].urls.regular}')";}</script>`;
+    // document.body.style.backgroundImage = `"url()"`;
+} 
 
 
 function displayResults(weather){
